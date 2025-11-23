31f764361bb0459f8492dfc80f232652230cde45
kujtimpllana <kujtimpllanadev@gmail.com>
2025-11-23 09:26:50 +0100

    Change db URI & Add deployments, services, config & secrets, pv & pvcs...

--- api/config/db.js
+++ api/config/db.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(
+      `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.MONGO_DB_SERVER}:${process.env.DB_PORT}/${process.env.DB_NAME}?authSource=admin`
+    );
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
