9b04ea6971ce4f5c153f553262723f65527ce478
kujtimpllana <kujtimpllanadev@gmail.com>
2025-11-23 03:55:50 +0100

    Implement Back End

---
+++ api/routes/productRoutes.js
@@ -0,0 +1,31 @@
+import express from "express";
+import Product from "../models/Product.js";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { name, price, description } = req.body;
+    const newProduct = new Product({ name, price, description });
+    const savedProduct = await newProduct.save();
+    res.status(201).json(savedProduct);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.json({ message: "Product deleted" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+export default router;
